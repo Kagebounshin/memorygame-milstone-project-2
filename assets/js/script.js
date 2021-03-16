e9c9a810ac07956b28a6085991c607c47e61d3d6
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-03-16 12:09:48 +0000

    Added more styling to the layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@
 $(document).ready(function() {
 
     $('.cards').click(function() {
-        $('.cards').css("background-color", "green");
+        $('.cards').css("background-color", "#2121212b");
     });
     $('.cards').mouseleave(function() {
-        $('.cards').css("background-color", "#000");
+        $('.cards').css("background-color", "#b75353b8");
     });
 
 
