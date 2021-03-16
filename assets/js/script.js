74bcd528667655393b64fc75a6a4a5656f7d7442
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-03-16 09:50:45 +0000

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+
+    $('.cards').click(function() {
+        $('.cards').css("background-color", "green");
+    });
+    $('.cards').mouseleave(function() {
+        $('.cards').css("background-color", "#000");
+    });
+
+
+});
+
+
